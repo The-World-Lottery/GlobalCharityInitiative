16775d0a13a54e44cdbbf876833a0e675d1565ab
Emmett Peters <ejp8611@gmail.com>
2017-09-01 16:29:18 -0500

    added empty master.blade

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -11,26 +11,23 @@
  	<main class="container">
  	<h1>Login</h1>
         <form method="POST" action="/auth/login">
-    {!! csrf_field() !!}
+            {!! csrf_field() !!}
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
-
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
-
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
-    </div>
-
-    <div>
-        <button type="submit">Login</button>
-    </div>
-</form>
+            <div>
+                Email
+                <input type="email" name="email" value="{{ old('email') }}">
+            </div>
+            <div>
+                Password
+                <input type="password" name="password" id="password">
+            </div>
+            <div>
+                <input type="checkbox" name="remember"> Remember Me
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
     </main>
 
 @stop
