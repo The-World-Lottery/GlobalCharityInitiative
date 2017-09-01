fc286bc4dc8e8405c3d997707d8e9497f550c28f
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-09-01 16:30:23 -0500

    Filled out migration files

--- database/migrations/2017_09_01_201719_create_raffles_table.php
+++ database/migrations/2017_09_01_201719_create_raffles_table.php
@@ -13,7 +13,14 @@ class CreateRafflesTable extends Migration
     public function up()
     {
          Schema::create('raffles', function (Blueprint $table) {
-           
+            $table->increments('id');
+            $table->string('title');
+            $table->string('content');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('product');
+            $table->timestamps();
+            $table->dateTime('end_date');
         });
     }
 
--- database/migrations/2017_09_01_201931_create_raffle_entrants_table.php
+++ database/migrations/2017_09_01_201931_create_raffle_entrants_table.php
@@ -13,7 +13,11 @@ class CreateRaffleEntrantsTable extends Migration
     public function up()
     {
          Schema::create('raffleEntrants', function (Blueprint $table) {
-           
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('raffles_id')->unsigned();
+            $table->foreign('raffles_id')->references('id')->on('raffles');
         });
     }
 
