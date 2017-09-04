ed2db5bb942362effe4217ad1ff75bd80af3aea6
Emmett Peters <ejp8611@gmail.com>
2017-09-04 12:03:20 -0500

    got the suggestions controller correctly looking for the database, need to run migrations

--- app/Http/Controllers/SuggestionsController.php
+++ app/Http/Controllers/SuggestionsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Models\Suggestion;
+use App\Models\Vote;
+use Log;
+use App\User;
+use DB;
 
 class SuggestionsController extends Controller
 {
@@ -14,9 +19,18 @@ class SuggestionsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if($request->has('q')){
+            $q = $request->q;
+            $suggestions = Suggestion::search($q);    
+        } else {
+            $suggestions = Suggestion::with('user')->paginate(6);  
+        }
+
+
+        $data['suggestions']= $suggestions;
+        return view('suggestions.index',$data);
     }
 
     /**
@@ -26,7 +40,7 @@ class SuggestionsController extends Controller
      */
     public function create()
     {
-        //
+        return view('suggestions.create');
     }
 
     /**
@@ -37,7 +51,23 @@ class SuggestionsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, Suggestion::$rules);
+
+        $title = $request->input('title');
+        $content = $request->input('content');
+        $url = $request->input('url');
+        $suggestion = new Suggestion();
+        $suggestion->title = $title;
+        $suggestion->content = $content;
+        $suggestion->url = $url;
+        $suggestion->user_id = \Auth::id();
+        $suggestion->save();
+
+        $request->session()->flash('successMessage', 'Suggestion created');
+
+        Log::info("$title, $content, $url");
+
+        return redirect()->action('SuggestionsController@index');
     }
 
     /**
@@ -48,7 +78,27 @@ class SuggestionsController extends Controller
      */
     public function show($id)
     {
-        //
+        $suggestion = Suggestion::find($id);
+
+        if(!$suggestion){
+            abort(404);
+        }
+
+        $data['suggestion'] = $sugegstion;
+        return view('suggestions.show',$data);
+    }
+
+    public function userssuggestions($id)
+    {   
+        if(User::find($id) ===null)
+        {
+            abort(404);
+        };
+
+        $suggestions = User::find($id)->suggestions;
+
+        $data['suggstions'] = $suggestions;
+        return view('suggstions.userssuggestions',$data);
     }
 
     /**
@@ -59,7 +109,62 @@ class SuggestionsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $suggestion = Suggestion::find($id);
+
+        if(\Auth::id() == $suggestion->user_id){ 
+            if(!$suggestion){
+                abort(404);
+            }
+            $data['suggestion'] = $suggestion;
+            return view('suggestions.edit',$data);
+        } else {
+            header('Location:/suggestions');
+        }
+    }
+
+    public function upvote($id)
+    {
+        $suggestion = Suggestion::find($id);
+        $user_id = \Auth::id();
+        $suggestion_id = Suggestion::find($id)->id;
+        $voteCount = Vote::select('vote')->where('suggestion_id',$suggestion_id)->get()->count();
+
+
+
+
+        dd(DB::table('votes')->select('vote')->where('suggestion_id',$suggestion_id)->sum('vote'));
+
+
+        $currVotes = Vote::where('suggestion_id',$id)->where('user_id',$user_id)->get();
+        // if($currVotes->isEmpty()){
+            $vote = new Vote;
+            $vote->user_id = $user_id;
+            $vote->suggestion_id = $suggestion_id;
+            $vote->vote = 1;
+            $vote->save();
+        // };
+
+        $data['suggestion'] = $suggestion;
+        return view('suggestions.show',$data);
+    }
+
+    public function downvote($id)
+    {
+        $suggestion = Suggestion::find($id);
+        $user_id = \Auth::id();
+        $suggestion_id = Suggestion::find($id)->id;
+        $currVotes = Vote::where('suggestion_id',$id)->where('user_id',$user_id)->get();
+        // if($currVotes->isEmpty()){
+            $vote = new Vote;
+            $vote->user_id = $user_id;
+            $vote->suggestion_id = $suggestion_id;
+            $vote->vote = -1;
+            $vote->save();
+        // };
+
+
+        $data['suggestion'] = $suggestion;
+        return view('suggestions.show',$data);
     }
 
     /**
@@ -71,7 +176,23 @@ class SuggestionsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, Suggestion::$rules);
+
+        $suggestion = Suggestion::find($id);
+
+        if(!$suggestion){
+            abort(404);
+        }
+
+        $suggestion->title = $request->title;
+        $suggestion->content =$request->content;
+        $suggestion->url =$request->url;
+        $suggestion->user_id = 1;
+        $suggestion->save();
+
+        $request->session()->flash('successMessage', 'Suggestion updated');
+
+        return \Redirect::action('SuggestionsController@show', $suggestion->id);
     }
 
     /**
@@ -80,8 +201,16 @@ class SuggestionsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        //
+        $suggestion = Suggestion::find($id);
+
+        if(!$suggestion){
+            abort(404);
+        }
+
+        $suggestion->delete();
+        $request->session()->flash('successMessage', 'Suggestion deleted');
+        return redirect()->action('SuggestionsController@index');
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('layouts.master');
 });
 
-Route::resource('posts', 'PostsController');
+Route::resource('suggestions', 'SuggestionsController');
 
 // Login routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
