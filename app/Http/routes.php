7aa3ef9d449587c6ddccb4726184c51a66b2d3fa
Emmett Peters <ejp8611@gmail.com>
2017-09-05 10:23:06 -0500

    commit before merge

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,6 +24,7 @@ Route::resource('suggestions', 'SuggestionsController');
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 
 Route::post('auth/login', 'Auth\AuthController@postLogin');
+
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
