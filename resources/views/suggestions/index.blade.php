508d1d9ea52ef0fbe045ae5236e641bea4b6efc0
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-09-05 09:43:07 -0500

    fixed views and routes for suggestions

--- resources/views/suggestions/index.blade.php
+++ resources/views/suggestions/index.blade.php
@@ -14,11 +14,11 @@
 
 		@foreach($suggestions as $suggestion)
 			<h1>{{$suggestion->title}}</h1>
-			{{-- <a href="{{action('suggestionsController@upvote',$suggestion->id)}}"><span class="glyphicon glyphicon-thumbs-up"></span></a>
-            <a href="{{action('suggestionsController@downvote',$suggestion->id)}}"><span class="glyphicon glyphicon-thumbs-down"></span></a> --}}
+			<a href="{{action('SuggestionsController@upvote',$suggestion->id)}}"><span class="glyphicon glyphicon-thumbs-up"></span></a>
+            <a href="{{action('SuggestionsController@downvote',$suggestion->id)}}"><span class="glyphicon glyphicon-thumbs-down"></span></a>
 			<p>{{$suggestion->content}}</p>
 			<p>By {{$suggestion->user->name}}</p>
-			{{-- <a href="{{ action('suggestionsController@show', $suggestion->id) }}">See More</a> --}}
+			<a href="{{ action('SuggestionsController@show', $suggestion->id) }}">See More</a>
 		@endforeach
 		<br>
 
