d5bf5790b747139ec4a129890d335b86fd022eff
Cody <cody@hastings.email>
2017-09-05 23:28:25 -0500

    the beginnings of styling edit fields for users

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,13 +8,13 @@
 
 @section('divHead')
 
-<span>Edit your User</span>
+<span>Edit Information</span>
 
 @stop
 
 @section('content')
 
-	<main class="container">
+	<main class="container" style="max-width:100%;float:left;">
 		<h1>Edit User</h1>
 		<form method="POST" action="{{ action('UsersController@update', $User->id)}}">
 			{!! csrf_field() !!}
@@ -22,14 +22,29 @@
 
 
 			{!! $errors->first('name', '<span class="help-block">:message</span>')!!}
-			<input type="text" name="name" id="name" value="{{ old('name') }}" placeholder="name">
+			<a class="btn btn-default active btn-block" data-toggle="collapse" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne" >
+				<p style="text-align: left"> <span style="padding-right:40%"><strong>Name</strong></span>{{$User->name}}<span class="glyphicon glyphicon-pencil" style= "padding-left:40%">Edit</span></p>
+			</a>
+			<div class="collapse" id="collapseOne">
+				<input type="text" name="name" id="name" value="{{$User->name}}" placeholder="{{$User->name}}">
+			</div>
 
 
 			{!! $errors->first('username', '<span class="help-block">:message</span>')!!}
-			<input type="text" name="username" id="username" value="{{ old('username') }}" placeholder="username">
+			<a class="btn btn-default active btn-block" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" >
+				<p style="text-align: left"> <span style="padding-right:40%"><strong>User Name</strong></span>{{$User->username}}<span class="glyphicon glyphicon-pencil" style= "padding-left:40%">Edit</span></p>
+			</a>
+			<div class="collapse" id="collapseTwo">
+				<input type="text" name="username" id="username" value="{{$User->username}}" placeholder="username">
+			</div>
 
 			{!! $errors->first('email', '<span class="help-block">:message</span>')!!}
-			<input type="text" name="email" id="email" value="{{ old('email') }}" placeholder="email">
+			<a class="btn btn-default active btn-block" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree" >
+				<p style="text-align: left"> <span style="padding-right:40%"><strong>Email</strong></span>{{$User->email}}<span class="glyphicon glyphicon-pencil" style= "padding-left:40%">Edit</span></p>
+			</a>
+			<div class="collapse" id="collapseThree">
+				<input type="text" name="email" id="email" value="{{$User->email}}" placeholder="email">
+			</div>
 
 
 
@@ -39,7 +54,12 @@
 			<input type="text" name="image" id="image" value="{{ old('image') }}" placeholder="image"> --}}
 
 			{!! $errors->first('phone_number', '<span class="help-block">:message</span>')!!}
-			<input type="text" name="phone_number" id="phone_number" value="{{ old('phone_number') }}" placeholder="phone_number">
+			<a class="btn btn-default active btn-block" data-toggle="collapse" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour" >
+				<p style="text-align: left"> <span style="padding-right:40%"><strong>Phone Number</strong></span>{{$User->email}}<span class="glyphicon glyphicon-pencil" style= "padding-left:40%">Edit</span></p>
+			</a>
+			<div class="collapse" id="collapseFour">
+				<input type="text" name="phone_number" id="phone_number" value="{{$User->phone_number}}" placeholder="phone_number">
+			</div>
 
 
 {{-- 		PLACEHOLDER FOR UPDATING PASSWORD
