692f36fec9d8c04b32428c50651af6b4a4e55a1d
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-09-06 14:21:35 -0500

    Added seeder files

---
+++ database/seeds/RafflesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RafflesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
