58c4d977062fbd543ffc0f6fdfd88f6e7f1c9124
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-09-12 15:03:39 -0500

    updated game seeders

--- database/seeds/LotterysTableSeeder.php
+++ database/seeds/LotterysTableSeeder.php
@@ -9,14 +9,19 @@ class LotterysTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i = 0;$i <= 20;$i++) {
+        for($i = 0;$i <= 23;$i++) {
 
        	$lottery = new \App\Models\Lottery();
         $lottery->title = 'Lottery #'.$i;
         $lottery->content = 'for charity';
         $lottery->init_value = 1000 * $i + 1000;
         $lottery->current_value = 1000 * $i + 1000;
-        $lottery->end_date = '2017-09-11 1'. $i .':00:00';
+        if($i < 9){
+        	$lottery->end_date = date("Y-m-d ") .'0'. $i .':00:00';
+    	}
+    	else{
+    		$lottery->end_date = date("Y-m-d ") . $i .':00:00';
+    	}
         $lottery->user_id = 1;
         $lottery->save();
     	}
