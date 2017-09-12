a341ab900d2b951d27dbfa47f08b7ec9e4a36c35
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-09-12 10:59:59 -0500

    add migration for winner ids

---
+++ database/migrations/2017_09_12_154558_WinnersCircle.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class WinnersCircle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table('raffles', function (Blueprint $table) {
+            $table->integer('winner_id')->unsigned();
+            $table->foreign('winner_id')->references('id')->on('users');
+        });
+          Schema::table('lotteries', function (Blueprint $table) {
+            $table->integer('winner_id')->unsigned();
+            $table->foreign('winner_id')->references('id')->on('users');
+        });
+           Schema::table('the_world_lotteries', function (Blueprint $table) {
+            $table->integer('winner_id')->unsigned();
+            $table->foreign('winner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('raffles', function (Blueprint $table) {
+            $table->dropColumn('winner_id');
+        });
+        Schema::table('lotteries', function (Blueprint $table) {
+            $table->dropColumn('winner_id');
+        });
+        Schema::table('the_world_lotteries', function (Blueprint $table) {
+            $table->dropColumn('winner_id');
+        });
+    }
+}
