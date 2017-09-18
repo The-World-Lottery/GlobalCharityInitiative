d6d6f546007d4cb9d783152919c62082c189a9bf
Avery Hankins <averyhankins@Averys-MacBook-Pro.local>
2017-09-18 17:20:04 -0500

    new navbar

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -1,7 +1,6 @@
 <div class="row" id="header">
 	<div class="col col-xs-12 borderOpac" id="head">
 		<div class="row">
-
 			<div id="googlepos" style="background-color:#161554;width:100%;display:flex;justify-content:center;">
 				<div>
 					<div style="color:white;" id="google_translate_element"></div><script type="text/javascript">
@@ -14,73 +13,49 @@
 			<div class="col-xs-12">
 
 				<h2 id="headLogo" style="font-family: 'Racing Sans One', cursive;color:lightblue;font-size:2em;text-align:center;">The World Lottery For Charity</h2>
-
 			</div>
-			<div class="col-sm-4">
-				@if (Auth::check())
-					<span style="padding-right:1em" class="headUN">{{Auth::user()->name}}</span>
+			<div style="text-align:center" class="col-xs-12">
+				@if (Auth::check())Logged In As<br>
+					<span style="margin-left:3px;" class="headUN">{{Auth::user()->name}}</span>
 					<div class="img-circle">
-						<img src='{{substr(Auth::user()->image,1,-1)}}' id="profImg">
+						<a class="" id="white" href="{{action('UsersController@show' , Auth::id())}}"><img src='{{Auth::user()->image}}' id="profImg"></a>
 					</div>
 				@else
-					<span style="padding-right:1em" class="headUN">Not Logged In</span>
+		
+				<a class="" id="white" href="{{action('Auth\AuthController@getLogin')}}">Login/Register</a>
 					
 				@endif
-				
+								
 			</div>
 			
 		</div>
 	</div>
 </div>
-<nav class="navbar navbar-default btn-custom" id="mainNav">
-  <div class="container-fluid borderOpac">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-      </button>
-    </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    
-      <ul style="display:flex;justify-content:space-around;" class="nav navbar-nav">
-		<li><a class="navLink btn-custom" id="white" href="{{action('TheWorldLotterysController@index')}}">The World Lottery</a></li>
-		<li><a class="navLink btn-custom" id="white" href="{{action('LotteriesController@index')}}">Lottos</a></li>
-		<li><a class="navLink btn-custom" id="white" href="{{action('RafflesController@index')}}">Raffles</a></li>
-		<li><a class="navLink btn-custom" id="white" href="{{action('CurrencyConversionController@index')}}">Currency Conversions</a></li>
-		<li><a class="navLink btn-custom" id="white" href="{{action('SuggestionsController@index')}}">Suggestion Box</a></li>
-		<li><a class="navLink btn-custom" id="white" href="{{action('AboutUsController@index')}}">About Us</a></li>
-		@if (Auth::check())
-    	<li><a class="navLink btn-custom" id="white" href="{{action('UsersController@show' , Auth::id())}}">Profile</a></li>
-		@else
-    	<li><a class="navLink btn-custom" id="white" href="{{action('Auth\AuthController@getLogin')}}">Login/Register</a></li>
-		@endif
-      </ul>
-
-   <!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
+<div class="w3-sidebar w3-bar-block w3-black">
+	<a class="w3-bar-item w3-button" id="white" href="{{action('TheWorldLotterysController@index')}}">The World Lottery</a>
+	<button onclick="myFunction('Demo1')" class="w3-button w3-block w3-left-align">Games</button>
+	<div id="Demo1" class="w3-bar-block w3-hide  w3-card-4">
+	<a class="w3-bar-item w3-button" id="white" href="{{action('LotteriesController@index')}}">Lottos</a>
+	<a class="w3-bar-item w3-button" id="white" href="{{action('RafflesController@index')}}">Raffles</a>
+	</div>
+	<a class="w3-bar-item w3-button" id="white" href="{{action('CurrencyConversionController@index')}}">Currency Conversions</a>
+	<a class="w3-bar-item w3-button" id="white" href="{{action('SuggestionsController@index')}}">Suggestion Box</a>
+	<a class="w3-bar-item w3-button" id="white" href="{{action('AboutUsController@index')}}">About Us</a>
+  
+</div>
 
 @if(Auth::check() && Auth::user()->is_admin)
-<nav class="navbar navbar-default btn-custom" id="mainNav">
-  <div class="container-fluid borderOpac">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-      </button>
-    </div>
+<div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul style="display:flex;justify-content:space-around;" class="nav navbar-nav">
-		<li><a class="navLink btn-custom" id="white" href="{{action('LotteriesController@adminIndex')}}">Manage Lotteries</a></li>
-		<li><a class="navLink btn-custom" id="white" href="{{action('RafflesController@adminIndex')}}">Manage Raffles</a></li>
-		<li><a class="navLink btn-custom" id="white" href="{{action('UsersController@index')}}">Manage Users</a></li>
-		<li><a class="navLink btn-custom" id="white" href="{{action('SuggestionsController@adminIndex')}}">Manage Suggestions</a></li>
+    Admin Functions:
+      <ul  class="">
+		<li><a class="" id="white" href="{{action('LotteriesController@adminIndex')}}">Manage Lotteries</a></li>
+		<li><a class="" id="white" href="{{action('RafflesController@adminIndex')}}">Manage Raffles</a></li>
+		<li><a class="" id="white" href="{{action('UsersController@index')}}">Manage Users</a></li>
+		<li><a class="" id="white" href="{{action('SuggestionsController@adminIndex')}}">Manage Suggestions</a></li>
       </ul>
 
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+</div>
+   
 @endif
-
