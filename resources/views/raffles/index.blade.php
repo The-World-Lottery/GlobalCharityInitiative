48577c0bc99b4c4e4384bc23951059e27e3b7d2a
Emmett Peters <ejp8611@gmail.com>
2017-09-18 14:24:42 -0500

    style changes

--- resources/views/raffles/index.blade.php
+++ resources/views/raffles/index.blade.php
@@ -10,7 +10,6 @@
 
 <h2 id="hoverTrigger"><strong>All Raffles</strong></h2>
 <p id="hoverSumm" hidden class="suggBox">Raffles are created by a company who wishes to advertise through us by donating a product or service they provide. Our hope is that we will become large enough for celebrities to donate "A day with a fan" or something of the like. When the date/time of each Raffle is reached, one user will be randomly selected from the pool of entrys. This ensures there will always be a winner. Each ticket you have to the lottery increases your chances of winning. Good Luck!</p>
-		{!! $raffles->appends(Request::except('page'))->render() !!} 
 
 @stop
 
@@ -54,11 +53,13 @@
 
 		@endforeach
 		<br>
+		<br>
 
 			{{-- get cody to help this work for each raffle --}}
 			{{-- <span class="raffleClock" data-clock-id="{{$raffle->end_date}}"></span> --}}
 	
 
+		{!! $raffles->appends(Request::except('page'))->render() !!} 
 	</main>
 
 @stop
